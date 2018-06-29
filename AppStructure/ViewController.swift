dce7b52009a6e229622fd0f74dc7d525e94949a6
datdn <datdn@ominext.com>
2018-06-29 14:21:08 +0700

    Initial Commit

---
+++ AppStructure/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AppStructure
+//
+//  Created by Ominext on 6/29/18.
+//  Copyright © 2018 mobile. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
